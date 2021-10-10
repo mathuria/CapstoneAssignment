e4b350614dc1d0dd7f0b47cad86f4496c5025d60
mathuria <73874709+mathuria@users.noreply.github.com>
2021-10-10 22:45:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,57 +1,59 @@
 from flask import Flask
-from flask.ext.httpauth import HTTPBasicAuth
-from flask.ext.api import status
 from json import dumps
 from flask import request
 from flask import Response
 
 app = Flask(__name__)
 
-auth = HTTPBasicAuth()
-
 users = {
     "admin": "root",
 }
 
-@auth.get_password
 def get_pw(username):
     if username in users:
         return users.get(username)
     return None
 
-@app.route('/secret')
-@auth.login_required
-def secret_page():
-    return Response("Hello, %s!" % auth.username(), 201, mimetype='application/json')
-
 @app.route('/')
 def root_test():
     return 'index'
 
+@app.route('/secret')
+def secret_page():
+    return Response("Hello", 201, mimetype='application/json')
+
 @app.route('/user', methods=['GET'])
 def get_users():
-	return dumps(users)	
+	return dumps(users)
 
 @app.route('/user/<string:username>', methods=['GET'])
 def get_profile(username):
 	return username + " profile"
 
-@app.route('/user/<string:username>', methods=['POST'])
-def add_user(username):
-	users[username] = request.form['pwd']
-	resp = Response(dumps(username), status=200, mimetype='application/json')
+@app.route('/user/add/<string:username>/pwd/<string:password>', methods=['POST'])
+def add_user(username, password):
+	users[username] = password
+	return Response("User added" , status=200, mimetype='application/json')
+
+@app.route('/user/<string:username>/pwd/<string:password>', methods=['GET'])
+def authenticate_user(username, password):
+	resp = Response("Pasword changed" , status=200, mimetype='application/json')
+	if not username in users.keys():
+		resp = Response("User " + username + " not found!" , status=403, mimetype='application/json')
+	elif users[username] == password:
+		resp = Response("Authentication Successful!!", 201, mimetype='application/json')
+	else:
+		resp = Response("Authentication Failed!!", 405, mimetype='application/json')
 	return resp
 
-@app.route('/user/<string:username>/pwd', methods=['PUT'])
-def change_password(username):
+@app.route('/user/<string:username>/pwd/<string:password>', methods=['POST'])
+def change_password(username, password):
 	resp = Response("Pasword changed" , status=200, mimetype='application/json')
-	if not users.has_key(username):
+	if not username in users.keys():
 		resp = Response("User " + username + " not found!" , status=403, mimetype='application/json')
 	else:
-		users[username] = request.form['pwd']
-
+		users[username] = password
 	return resp
 
 if __name__ == '__main__':
     app.run()
-
